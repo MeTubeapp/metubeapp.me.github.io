8d3d1c19f9c3d68462f3c14bef0b7b8aa7154f5b
MeTubeapp <62765738+MeTubeapp@users.noreply.github.com>
2021-06-26 19:19:03 +0530

    new

---
+++ script.js
@@ -0,0 +1,11 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "shivika" && b=="shivika")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
